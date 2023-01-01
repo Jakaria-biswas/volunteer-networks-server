4b8e97156f4d8d4743d114305521552684cd3a4f
Jakaria Biswas <contactjakaria95@gmail.com>
2023-01-02 00:53:06 +0600

    ready api

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 const port = 3400;
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.user}:${process.env.password}@cluster0.6emt6xb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -21,6 +21,7 @@ async function run() {
 
   const volunteer = client.db("volunteer");
   const event = volunteer.collection("event")
+  const donation = volunteer.collection("donation")
   try {
     // await client.connect();
     // console.log("database connected")
@@ -32,19 +33,85 @@ async function run() {
       res.send(result)
     })
 
-    /// get event through admin
 
+    /// get event through admin
     app.get("/event", async(req, res) => {
 
             const query = {}
-            const cursor = event.find({})
+            const cursor = event.find(query)
             const result =  await cursor.toArray()
             res.send(result)
          
     })
 
+    /// specific event delete through admin
 
-     
+    app.delete("/delete/:id", async(req, res) => {
+          
+          const id = req.params.id;
+          const query = {_id: ObjectId(id)}
+
+          const result = await event.deleteOne(query)
+          res.send(result)
+    })
+
+    // load single event through admin for update
+
+    app.get('/event/:id', async(req, res) => {
+         const id = req.params.id;
+         const query = {_id: ObjectId(id)}
+         const result = await event.findOne(query)
+         res.send(result)
+    })
+
+      /// update single event through admin
+
+      app.put("/event/:id", async (req,res) => {
+          
+             const id = req.params.id;
+             const filter = {_id: ObjectId(id)}
+             const options = { upsert: true };
+             const doc = {
+               $set:{
+                    image: req.body.image,
+                    title:req.body.title
+               }
+             };
+              const result = await event.updateOne(filter,doc,options)
+              res.send(result)
+      })
+
+
+      /// donation book for user and get single event detail 
+
+      app.get("/book/:id", async (req, res) => {
+            
+              const  id = req.params.id;
+              const query ={_id: ObjectId(id)}
+              const result = await event.findOne(query)
+              res.send(result)
+      })
+
+      //// store data from user for donation
+      
+      app.post("/bookDonate", async (req,res) => {
+            
+            const data = req.body;
+            const result = await donation.insertOne(data)
+            res.send(result)
+      })
+
+      //// per user donation book load  through email
+
+      app.get("/donation", async  (req, res) => {
+            const query = req.query.email;
+            const email = {donerEmail: query}
+            const cursor =  donation.find(email)
+            const result = await cursor.toArray()
+            res.send(result)
+            console.log(email)
+      })
+
   }
   finally {
     //   await client.close();
